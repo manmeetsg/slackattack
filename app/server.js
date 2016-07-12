264b15de0372c83f76ebd88a81b3d16f4d5abc8b
Manmeet <manmeetsgujral@gmail.com>
2016-07-12 03:26:16 -0400

    Edited return results

--- app/server.js
+++ app/server.js
@@ -94,6 +94,7 @@ controller.hears(['food'], ['direct_message', 'direct_mention', 'mention'],
     }
     con.next();
     con.say('Here is your top result: ');
+    con.next();
     con.say({
       attachments: [
         {
